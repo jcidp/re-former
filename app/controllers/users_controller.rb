af787e783c2d7e7153cd684672d8e60bc49a0acb
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-03-30 20:11:05 -0600

    Build user edit & update actions & view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,24 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      redirect_to new_user_path
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
